4144efcf87d481266c310a477a98cb81b305696c
saikumarreddy mopuri <saikumarmopuri8639@gmail.com>
2024-08-24 20:00:15 +0530

    connection of mongodb

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export  const DB_NAME = "BACKEND"
+
+
+
+
+ 
+// const constants = async ()  => {
+//     try {
+//         const DB_NAME = "BACKEND"
+         
+//     } catch (error) {
+         
+//     }
+// }
+// export default constants;
+  
+ 
