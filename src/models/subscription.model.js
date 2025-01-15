4282047c9fe4759c6821ce79e9f018bea666319d
saikumarreddy mopuri <saikumarmopuri8639@gmail.com>
2025-01-15 23:15:54 +0530

    add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,12 @@
+import mongoose, {Schema} from "mongoose"
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // ONE WHO IS SUBSCRIBING
+        ref: "User"
+    },
+    channel: {
+        type: Schema.Types.ObjectId, // ONE to whom 'subscriber ' is subscribing
+        ref: "User"
+    }
+},{timestamps: true})
